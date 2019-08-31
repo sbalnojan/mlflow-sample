808c425c15ea51aea093c8ed51ea4dd8771ecc84
Sven Balnojan <sven.balnojan@mercateo.com>
2019-08-31 21:15:15 +0200

    add logging example

---
+++ log_sth.py
@@ -0,0 +1,20 @@
+import os
+from mlflow import log_metric, log_param, log_artifact, set_tracking_uri
+
+MY_URI = "http://52.59.230.4:5000"
+
+if __name__ == "__main__":
+    set_tracking_uri(MY_URI)
+
+    # Log a parameter (key-value pair)
+    log_param("param1", 5)
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", 1)
+    log_metric("foo", 2)
+    log_metric("foo", 3)
+
+    # Log an artifact (output file)
+    with open("output.txt", "w") as f:
+        f.write("Hello world!")
+    #log_artifact("output.txt")
